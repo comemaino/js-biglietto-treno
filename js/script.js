0ca961ee1e44924226f7715105f444336700acf3
comemaino <mat.maino@gmail.com>
2022-04-07 15:03:56 +0200

    Add: train ticket price calculator with switch

--- js/script.js
+++ js/script.js
@@ -4,12 +4,24 @@ const defaultPrice = tripKms * 0.21;
 
 let finalPrice;
 
-if (userAge < 18) {
-	finalPrice = defaultPrice * 0.8;
-} else if (userAge > 65) {
-	finalPrice = defaultPrice * 0.6;
-} else {
-	finalPrice = defaultPrice;
+// if (userAge < 18) {
+// 	finalPrice = defaultPrice * 0.8;
+// } else if (userAge > 65) {
+// 	finalPrice = defaultPrice * 0.6;
+// } else {
+// 	finalPrice = defaultPrice;
+// }
+
+switch (true) {
+	case userAge < 18:
+		finalPrice = defaultPrice * 0.8;
+		break;
+	case userAge > 65:
+		finalPrice = defaultPrice * 0.6;
+		break;
+	default:
+		finalPrice = defaultPrice;
+		break;
 }
 
 document.getElementById("ticket-price").innerHTML = finalPrice.toFixed(2);
